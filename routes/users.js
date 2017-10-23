b4dbaf3cf2dd49709b4d15eca56e78c43f0c5533
mikha <juliandri.mikhael@gmail.com>
2017-10-24 03:19:01 +0700

    register function done

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,34 @@ const checker = require('../helpers/checker');
 
 
 /* GET users listing. */
+router.post('/register', function(req, res, next){ // ini untuk register user di dalam postman dengan email
 
+  let email = req.body.email;
+  let password = req.body.password;
+  let retypepassword = req.body.retypepassword;
+
+
+  if (!(email && password && retypepassword)) {
+    return res.json({success: false, message: 'authenticate failed. make sure u input the email and password'});
+  }else if (password != retypepassword) {
+    return res.json({success: false, message: 'authenticate failed. make sure u input the email and password'})
+  }
+
+  var user = new User({
+    email: email,
+    password: password
+  })
+  user.save(function(err) {
+    if (err) throw err;
+    console.log(user, 'data');
+      let token = jwt.sign({id: user._id, email: user.email}, config.secretkey, {
+        expiresIn: 86400
+      })
+      res.json({ //hasil yang di tampilkan oleh json berbentuk objek
+        data: {email: user.email},
+        token: token
+      })
+  })
+})
 
 module.exports = router;
