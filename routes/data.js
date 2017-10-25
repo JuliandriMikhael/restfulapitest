09e74d5b10ca09807fcecc551fd2c51146c23a58
mikha <juliandri.mikhael@gmail.com>
2017-10-26 02:28:50 +0700

    add data router

--- routes/data.js
+++ routes/data.js
@@ -3,3 +3,10 @@
 const express = require('express');
 const router = express.Router();
 const Data = require("../models/data")
+
+router.post('/input', function(req, res){ // ini itu cuma percobaan alias prototype
+  console.log(req.body.letter);
+  var data = new Data({ //router untuk setup dan login si user
+     letter: req.body.letter,
+     frequency: req.body.frequency
+  })
