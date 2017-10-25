a8964443a4d7857f2240616da80c9c022ff633bf
mikha <juliandri.mikhael@gmail.com>
2017-10-26 02:29:41 +0700

    edit add data router

--- routes/data.js
+++ routes/data.js
@@ -4,9 +4,16 @@ const express = require('express');
 const router = express.Router();
 const Data = require("../models/data")
 
-router.post('/input', function(req, res){ 
+router.post('/input', function(req, res){
   console.log(req.body.letter);
   var data = new Data({
      letter: req.body.letter,
      frequency: req.body.frequency
   })
+  data.save(function(err){
+    if (err) throw err;
+      console.log('User saved succesfully'); // ini fungsi kalo dia bisa login dan ngirim pesan
+      res.json({
+        success: true,
+        message: "data has been added",
+        data:{
