780482c3bfec8946948e9837eee90f7d5e5c54df
mikha <juliandri.mikhael@gmail.com>
2017-10-25 13:36:30 +0700

    edit index router

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-  secretkey: "juliandrimikhael", //untuk password
+  secretkey: "juliandrimikhael", //untuk token supaya dia diacak 
   database: "mongodb://localhost/restfultestdb" //nama bisa custom sesuai keinginan
 }
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/setup', function(req, res){
+  var mikha = new User({
+    username: "mikha",
+    password: '1234'
+  })
+  mikha.save(function(err) {
+    if (err) throw err;
+      console.log('user saved successfully');
+      res.json({success: true})
+  })
+})
+
 module.exports = router;
